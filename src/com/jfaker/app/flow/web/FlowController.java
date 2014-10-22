e8a37296faf7310c902ebbda45d6e4506030bf1a
snakerflow <snakerflow@163.com>
2014-10-22 10:48:32 +0800

    增加统一启动或执行任务的路由方法

---
+++ src/com/jfaker/app/flow/web/FlowController.java
@@ -0,0 +1,106 @@
+package com.jfaker.app.flow.web;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.jfaker.framework.security.shiro.ShiroUtils;
+
+public class FlowController extends SnakerController {
+	public static final String PARA_PROCESSID = "processId";
+	public static final String PARA_ORDERID = "orderId";
+	public static final String PARA_TASKID = "taskId";
+	public static final String PARA_METHOD = "method";
+	public static final String PARA_NEXTOPERATOR = "nextOperator";
+	public static final String PARA_NODENAME = "nodeName";
+	public static final String PARA_CCOPERATOR = "ccOperator";
+	
+	/**
+	 * 处理流程启动或任务执行，并且将表单数据保存至实例、任务变量中
+	 * 变量类型根据表单字段的首字母决定，类型分别为:S字符型,I整形,L常整形,B布尔型,D日期型,N浮点型
+	 * 执行规则根据method的值决定，值分别为:0执行,-1驳回或跳转,1转主办,2转协办
+	 * 适用于演示流程，或者是无业务字段的正式流程
+	 */
+	@SuppressWarnings("unchecked")
+	public void process() {
+		Map<String, Object> params = new HashMap<String, Object>();
+		Enumeration<String> paraNames = getRequest().getParameterNames();
+		while (paraNames.hasMoreElements()) {
+			String element = paraNames.nextElement();
+			int index = element.indexOf("_");
+			if(index == -1) {
+				params.put(element, getPara(element));
+			} else {
+				char type = element.charAt(0);
+				String name = element.substring(index + 1);
+				Object value = null;
+				switch(type) {
+				case 'S':
+					value = getPara(element);
+					break;
+				case 'I':
+					value = getParaToInt(element);
+					break;
+				case 'L':
+					value = getParaToLong(element);
+					break;
+				case 'B':
+					value = getParaToBoolean(element);
+					break;
+				case 'D':
+					value = getParaToDate(element);
+					break;
+				case 'N':
+					value = Double.parseDouble(getPara(element));
+					break;
+				default:
+					value = getPara(element);
+					break;
+				}
+				params.put(name, value);
+			}
+		}
+		String processId = getPara(PARA_PROCESSID);
+		String orderId = getPara(PARA_ORDERID);
+		String taskId = getPara(PARA_TASKID);
+		String nextOperator = getPara(PARA_NEXTOPERATOR);
+		if (StringUtils.isEmpty(orderId) && StringUtils.isEmpty(taskId)) {
+			startAndExecute(processId, ShiroUtils.getUsername(), params);
+		} else {
+			int method = getParaToInt(PARA_METHOD, 0);
+			switch(method) {
+			case 0://任务执行
+				execute(taskId, ShiroUtils.getUsername(), params);
+				break;
+			case -1://驳回、任意跳转
+				executeAndJump(taskId, ShiroUtils.getUsername(), params, getPara(PARA_NODENAME));
+				break;
+			case 1://转办
+				if(StringUtils.isNotEmpty(nextOperator)) {
+					transferMajor(taskId, ShiroUtils.getUsername(), nextOperator.split(","));
+				}
+				break;
+			case 2://协办
+				if(StringUtils.isNotEmpty(nextOperator)) {
+					transferAidant(taskId, ShiroUtils.getUsername(), nextOperator.split(","));
+				}
+				break;
+			default:
+				execute(taskId, ShiroUtils.getUsername(), params);
+				break;
+			}
+		}
+		String ccOperator = getPara(PARA_CCOPERATOR);
+		if(StringUtils.isNotEmpty(ccOperator)) {
+			engine.order().createCCOrder(orderId, ccOperator.split(","));
+		}
+		redirect("/snaker/task/active");
+	}
+	
+	public void approval() {
+		
+		redirect("/snaker/task/active");
+	}
+}
