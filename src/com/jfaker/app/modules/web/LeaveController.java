aa364dab07083aaf72ebdfe66b9e9d0a4d94cca4
snakerflow <snakerflow@163.com>
2014-10-26 20:25:06 +0800

    增加详细注释说明

--- src/com/jfaker/app/modules/web/LeaveController.java
+++ src/com/jfaker/app/modules/web/LeaveController.java
@@ -7,8 +7,12 @@ import com.jfaker.framework.security.shiro.ShiroUtils;
 
 /**
  * 请假流程Controller
+ * 流程特点:多级审批流程、decision节点的表达式写法->${day > 2 ? 'transition5' : 'transition4'}
+ * 业务数据:
+ *     所有节点的业务数据均以json格式保存在order、task表的variable字段中
+ *     建议业务数据以独立的表保存，通过orderId来关联
  * @author yuqs
- * @since 0.1
+ * @since 1.0
  */
 public class LeaveController extends SnakerController {
 	/**
@@ -30,12 +34,15 @@ public class LeaveController extends SnakerController {
 			//如果orderId非空、taskId为空，则表示申请步骤已提交，此时可获取申请数据
 			//由于请假流程中的业务数据，是保存在任务表的variable字段中，所以通过flowData方法获取
 			//如果业务数据保存在业务表中，需要业务表的orderId字段来关联流程，进而根据orderId查询出业务数据
-			flowData(orderId, getPara("taskName"));
+			flowData(orderId, getPara(PARA_TASKNAME));
 			//返回申请的查看页面
 			render("applyView.jsp");
 		}
 	}
 	
+	/**
+	 * 部门经理审批路由方法
+	 */
 	public void approveDept() {
 		keepPara();
 		String orderId = getPara(PARA_ORDERID);
@@ -43,11 +50,14 @@ public class LeaveController extends SnakerController {
 		if(StringUtils.isNotEmpty(taskId)) {
 			render("approveDept.jsp");
 		} else {
-			flowData(orderId, getPara("taskName"));
+			flowData(orderId, getPara(PARA_TASKNAME));
 			render("approveDeptView.jsp");
 		}
 	}
 	
+	/**
+	 * 总经理审批路由方法
+	 */
 	public void approveBoss() {
 		keepPara();
 		String orderId = getPara(PARA_ORDERID);
@@ -55,7 +65,7 @@ public class LeaveController extends SnakerController {
 		if(StringUtils.isNotEmpty(taskId)) {
 			render("approveBoss.jsp");
 		} else {
-			flowData(orderId, getPara("taskName"));
+			flowData(orderId, getPara(PARA_TASKNAME));
 			render("approveBossView.jsp");
 		}
 	}
